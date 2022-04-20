b7b292eeb2e8ccf47c99195b2481bb87d14462f4
jangkyoungtae <zxs3710@naver.com>
2022-04-20 15:40:30 +0900

    detail page

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
 from pymongo import MongoClient
 import certifi
 
+app = Flask(__name__)
 ca = certifi.where()
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.zgm92.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -16,6 +14,16 @@ db = client.cafe
 def home():
     return render_template('index.html')
 
+@app.route('/detail', methods=["POST"])
+def detail():
+    cafe = request.form['cafe']
+    image = request.form['image']
+    data = {
+        'cafe':cafe,
+        'image':image
+    }
+    print(data)
+    return render_template('detail.html', data=data)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
