53ae48a5a72a70a8d3e6363f45eb48ab70007c92
jangkyoungtae <zxs3710@naver.com>
2022-04-20 17:37:41 +0900

    댓글 작성 및 불러오기

--- app.py
+++ app.py
@@ -25,12 +25,23 @@ def detail():
     print(data)
     return render_template('detail.html', data=data)
 
+
+@app.route('/post_comment', methods=["POST"])
 def post_comment():
     cafe = request.form['cafe']
     nickname = request.form['nickname']
     comment = request.form['comment']
     doc = {'cafe': cafe, 'nickname': nickname, 'comment': comment}
-    db.users.insert_one(doc)
+    db.comment.insert_one(doc)
     return jsonify({'msg':"댓글 작성 완료"})
+
+@app.route('/comment_list', methods=["GET"])
+def comment_list():
+    cafes = request.args.get('cafe')
+    cafe_list = list(db.comment.find({"cafe":{'$regex': cafes }}, {'_id': False}))
+    return jsonify({'cafe_list': cafe_list})
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
