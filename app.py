d155c02f43ba258951471b68d9e77c501afc6a34
jangkyoungtae <zxs3710@naver.com>
2022-04-26 15:58:34 +0900

    첫번째 페이지 완료 및 댓글 수정 삭제 완료

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import time
-
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -15,13 +12,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
 app = Flask(__name__)
+
 ca = certifi.where()
+
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.zgm92.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
                      tlsCAFile=ca)
+
 db = client.cafe
+
 
 
 @app.route('/')
@@ -33,12 +33,13 @@ def detail():
     cafe = request.form['cafe']
     image = request.form['image']
     code = request.form['code']
+    dats_list = list();
     data = {
         'cafe':cafe,
         'image':image,
         'code':code
     }
-    print(data)
+    dats_list.append(data)
     return render_template('detail.html', data=data)
 
 
@@ -47,7 +48,9 @@ def post_comment():
     cafe = request.form['cafe']
     nickname = request.form['nickname']
     comment = request.form['comment']
-    doc = {'cafe': cafe, 'nickname': nickname, 'comment': comment}
+    cafe_list = list(db.comment.find({"cafe": {'$regex': cafe}}, {'_id': False}))
+    count = len(cafe_list)+1
+    doc = {'num':count,'cafe': cafe, 'nickname': nickname, 'comment': comment}
     db.comment.insert_one(doc)
     return jsonify({'msg':"댓글 작성 완료"})
 
@@ -57,12 +60,25 @@ def comment_list():
     cafe_list = list(db.comment.find({"cafe":{'$regex': cafes }}, {'_id': False}))
     return jsonify({'cafe_list': cafe_list})
 
+@app.route('/comment_del', methods=["POST"])
+def comment_del():
+    num = request.form['num']
+    db.comment.delete_one({'num':int(num)})
+    return jsonify({'msg': "삭제되었습니다."})
+
+@app.route('/comment_update', methods=["POST"])
+def comment_update():
+    num = request.form['num']
+    comment= request.form['comment']
+    nickname = request.form['nickname']
+    db.comment.update_one({'num':int(num)}, {'$set': {'nickname': nickname,'comment':comment}})
+    return jsonify({'msg': "수정되었습니다.."})
+
 @app.route('/detail_info', methods=["GET"])
 def detail_info():
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "none"
     cafe_cade = request.args.get('code')
-    print(cafe_cade)
 
     url ='http://place.map.kakao.com/'+cafe_cade
 
@@ -83,6 +99,11 @@ def detail_info():
         "#mArticle > div.cont_essential > div.details_placeinfo > div:nth-child(5) > div > div > span")
     return jsonify({'address':address.text,'time':times.text,'homepage':homepage['href'],'tell':tell_num.text})
 
+@app.route('/cafe_list', methods=["GET"])
+def cafe_list():
+    cafes = request.args.get('city')
+    lists = list(db.cafe.find({"add":{'$regex': cafes }}, {'_id': False}))
+    return jsonify({'list': lists})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
